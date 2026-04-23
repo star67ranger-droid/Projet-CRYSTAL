e72ffddc97a3bdc93b0d7943d33db3ce2cd7d13f
star67ranger-droid <star67ranger@gmail.com>
2026-04-23 20:53:16 +0200

    TEST DEPLOYMENT

--- index.js
+++ index.js
@@ -561,7 +561,7 @@ const paycrystal = {
     }
 };
 
-// ─── Settings ─────────────────────────────────────────────────────────────────
+// ─── Settings ────────────────────────────────────────────────────────────────
 
 function buildSettingsContainer(interactionUserId) {
     const codes = getCodes();
